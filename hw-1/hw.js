d50518883fb4ce233236e715241118abf9333d71
HalimHamidov <halimjonikgmail.com>
2020-05-13 01:16:42 +0300

    fix: delete add HW19 js task

--- hw-1/hw.js
+++
@@ -1,9 +0,0 @@
-// Создать две переменные: name, admin
-
-let name, admin;
-// В переменную name запишите своё имя
-name = 'Halim';
-// В переменную admin запишите значение переменной name
-admin = name;
-// выведите на экран admin, через функцию alert
-alert(admin);
