ba1191d4aba7aa238b4c137862e4f83eb5d6671e
Halim <46631802+HalimHamidov@users.noreply.github.com>
2020-05-25 17:05:16 +0300

    Update ДЗ2.js

--- ДЗ2.js
+++ ДЗ2.js
@@ -43,7 +43,7 @@ console.log(sum(5)(-1));
 // Измените код так, чтобы console.log выводил последовательно символы 'П т и ц а'
 const bird = ['П', 'т', 'и', 'ц', 'а'];
 
-for (var index = 0; index < bird.length; index++) {
+for (let index = 0; index < bird.length; index++) {
     console.log(bird[index]);
 }
 // for (var index = 0; index < bird.length; index++) {
