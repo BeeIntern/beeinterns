d63f74c3e9b180d8cb6f5c61a6fdbaf1f0e7628f
Halim <46631802+HalimHamidov@users.noreply.github.com>
2020-06-28 11:27:48 +0300

    Update hw.js
    according to remarks of the mentor

--- hw-3/hw.js
+++ hw-3/hw.js
@@ -5,15 +5,13 @@
     используя подходящие оператоы
     следуя рекомендациям по именованию переменных
 */
-
-let age = 20;
+const age = 20;
 let text = `Ваш возраст: ${age} лет`;
 alert(text); // Должен вывести: Ваш возраст: 20 лет
 
-
-let hours = 12;
-let minutes = 45;
-hours = hours + 1;
+const hours = 12;
+const minutes = 45;
+hours++;
 let textWithTime = `Текущее время: ${hours} : ${minutes}`;
 
 alert(textWithTime); // Должен вывести: Текущее время: 13:45
